65ecbd6650a0ed5e2d0d14aaadced031c220f9fd
Thomas Exler <thomas.exler@hotmail.com>
2023-09-15 19:42:16 +0200

    Added frequent attacks on enemies in range for towers

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -1,9 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
+    public static EventManager Instance { get; private set; }
+
+    public event Action<GameObject> OnEnemyDeath;
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +30,10 @@ public class EventManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void EnemyDeath(GameObject enemy)
+    {
+        OnEnemyDeath?.Invoke(enemy);
     }
 }
--- Assets/Scripts/TowerBehavior.cs
+++ Assets/Scripts/TowerBehavior.cs
@@ -6,8 +6,21 @@ public class TowerBehavior : MonoBehaviour
 {
 
     private float range;
-    private List<GameObject> enemiesInRange;
+    private List<GameObject> enemiesInRange = new List<GameObject> ();
     [SerializeField] private GameObject rangeIndicator;
+    private bool isEnemyInRange = false;
+    private float attackFrequency = 1f;
+
+
+    private void OnEnable()
+    {
+        EventManager.Instance.OnEnemyDeath += RemoveEnemyInRange;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.Instance.OnEnemyDeath -= RemoveEnemyInRange;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -21,14 +34,62 @@ public class TowerBehavior : MonoBehaviour
         
     }
 
+    IEnumerator AttackRoutine()
+    {
+        while (isEnemyInRange)
+        {
+            Attack();
+            yield return new WaitForSeconds(attackFrequency);
+        }
+        yield return new WaitForSeconds(attackFrequency);
+
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        enemiesInRange.Add(other.gameObject);
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("Found Enemy!");
+            AddEnemyInRange(other.gameObject);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        enemiesInRange.Remove(other.gameObject);
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("Lost Enemy!");
+            RemoveEnemyInRange(other.gameObject);
+        }
+    }
+
+    private void AddEnemyInRange(GameObject enemy)
+    {
+        enemiesInRange.Add(enemy);
+        CheckFirstEnemyInRange();
+    }
+
+    private void RemoveEnemyInRange(GameObject enemy)
+    {
+        enemiesInRange.Remove(enemy);
+        CheckLastEnemyInRange();
+    }
+
+    private void CheckFirstEnemyInRange()
+    {
+        if(enemiesInRange.Count == 1)
+        {
+            isEnemyInRange = true;
+            StartCoroutine(AttackRoutine());
+        }
+    }
+
+    private void CheckLastEnemyInRange()
+    {
+        if(enemiesInRange.Count == 0)
+        {
+            isEnemyInRange = false;
+        }
     }
 
     private void DisableRangeIndicator()
@@ -39,5 +100,14 @@ public class TowerBehavior : MonoBehaviour
     private void EnableRangeIndicator()
     {
         rangeIndicator.SetActive(true);
+    }
+
+    protected virtual void Attack()
+    {
+        Debug.Log("Attac!");
+        foreach(GameObject enemy in enemiesInRange)
+        {
+            Destroy(enemy);
+        }
     }
 }
