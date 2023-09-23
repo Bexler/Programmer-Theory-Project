35127ef6a0b6ea544aef2f75706664af0a12d1a3
Thomas Exler <thomas.exler@hotmail.com>
2023-09-23 17:23:23 +0200

    Added blue tower shockwave, shockwave is a sphere that appears when attacking and quickly expands up to range of tower before disappearing again

--- Assets/Scripts/Tower Scripts/BlueTowerBehavior.cs
+++ Assets/Scripts/Tower Scripts/BlueTowerBehavior.cs
@@ -6,13 +6,29 @@ using UnityEngine;
 public class BlueTowerBehavior : TowerBehavior
 {
 
+    [SerializeField] private GameObject shockwave;
+    [SerializeField] private float shockwaveSpeed = 10;
+
     private int damage = 1;
+    private bool isShockwaving;
 
     protected override void Start()
     {
         base.Start();
         attackFrequency = 1f;
         goldCost = 10;
+    }
+
+    private void Update()
+    {
+        if (isShockwaving)
+        {
+            shockwave.transform.localScale *= (1 + (shockwaveSpeed * Time.deltaTime));
+            if(shockwave.transform.localScale.x > 10)
+            {
+                DisableShockwave();
+            }
+        }
     }
 
     protected override void Attack()
@@ -25,12 +41,22 @@ public class BlueTowerBehavior : TowerBehavior
             
         }
         RemoveDefeatedEnemiesInRange();
+        EnableShockwave();
         
     }
 
-    private void TestCall()
+    private void EnableShockwave()
     {
-        
+        isShockwaving = true;
+        shockwave.SetActive(true);
+        shockwave.transform.localScale = Vector3.one;
+    }
+
+    private void DisableShockwave()
+    {
+        isShockwaving = false;
+        shockwave.SetActive(false);
+
     }
 
 }
